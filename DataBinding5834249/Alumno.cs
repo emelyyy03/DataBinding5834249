a4867218d4436d09b5e55f8c9904d3fc1ca56767
emelyyy03 <emelyyy03@outlook.com>
2024-07-20 01:10:16 -0600

    Agregar archivos de proyecto.

---
+++ DataBinding5834249/Alumno.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace DataBinding5834249
+{
+    internal class Alumno : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        string nombre = string.Empty;
+
+        public string Nombre
+        {
+            get => nombre;
+            set
+            {
+                if (nombre == value)
+                    return;
+                nombre = value;
+                onPropertyChanged(nameof(Nombre));
+                onPropertyChanged(nameof(MostrarNombre));
+            }
+        }
+
+        public string MostrarNombre => $"Nombre Ingresado: {Nombre}";
+
+        void onPropertyChanged(string nombre)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nombre));
+        }
+    }
+}
---
+++ DataBinding5834249/MainPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace DataBinding5834249
+{
+    public partial class MainPage : ContentPage
+    {
+        
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+    }
+
+}
